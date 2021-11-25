f586b452996d147de8da854b3d8180b2544e4420
Amber <martinez.amber1028@gmail.com>
2021-11-25 15:33:30 -0800

    commit tree 3b9f466e376842f2469056c64361555d71e84d4c

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function saturdayFun(activity="roller-skate") {
+function saturdayFun(activity = `roller-skate`){
   return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+const mondayWork = function(activity2 = `go to the office`) {
+  return `This Monday, I will ${activity2}.`
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
-  }
+function wrapAdjective(wrap = `*`){
+    function inner(adjective = `special`) {
+      return `You are ${wrap}${adjective}${wrap}!`
+    }
+    return inner
 }
